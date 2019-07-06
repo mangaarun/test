5d6f58d74aed66f321c0bdf3be5b1f176ea24241
mangaarun <mangaarun@gmail.com>
2019-07-06 10:42:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -11,8 +11,6 @@ void(0);}
 
 
 //prompt for login captcha
-javascript:{
-  var cap = prompt("Please enter captcha", "");
-  document.getElementById("ccMain_txtEnterCode").value = cap;
-  void(0);
+javascript:{$i=200;var intId = setInterval(function(){if($("#btnLogin").get(0).style.display != "none") {$("#btnLogin").click();clearInterval(intId);}
+    },$i);var cap = prompt("Please enter captcha", "");document.getElementById("ccMain_txtEnterCode").value = cap;void(0);
 }
